cd9f20356746a2341b6c62d34ff62d3990a462d1
saleeqmohammed <saleeqmohammed7@gmail.com>
2023-11-24 23:46:32 +0000

    states generated

--- data/states/stategen.py
+++ data/states/stategen.py
@@ -27,14 +27,12 @@ def trim_whitespace(image_path):
     bin_image = img_matrix.astype(int)
     #print(bin_image)
     non_zero_bounding = find_first_last_nonzero_indices(bin_image)
-    top_left = non_zero_bounding[0]
-    bottom_right=non_zero_bounding[1]
+    top_left = non_zero_bounding[0] #type:ignore
+    bottom_right=non_zero_bounding[1] #type:ignore
     x=top_left[0]
     y=top_left[1]
     x2 = bottom_right[0]
     y2 = bottom_right[1]
-    print(x)
-    print(y)
     
     #print(bin_image)
     return img[x:x2,y:y2]
@@ -46,18 +44,43 @@ def calculate_grid_size(img_map):
 
     return i
 
-def state_matrix_generator():
-    pass
+def state_matrix_generator(map_img):
+    grid= calculate_grid_size(map_img)
+    map_dim = map_img.shape
+    x_dim =map_dim[1]
+    y_dim =map_dim[0]
+    n_cells_x = math.ceil(x_dim/grid)
+    n_cells_y = math.ceil(y_dim/grid)
+    print(n_cells_x)
+    print(n_cells_y)
+    state_matrix =np.zeros((n_cells_x,n_cells_y))
+    #create a matrix that is the multiple of grid
+    pixels_x = n_cells_x*grid
+    pixels_y = n_cells_y*grid
+    padded_map = np.ones((pixels_y,pixels_x), dtype=np.uint8) * 255 # type: ignore
+    padded_map[:y_dim,:x_dim] = map_img
+    image_bgr = cv2.cvtColor(padded_map, cv2.COLOR_GRAY2BGR)
+    for cell_y in range(n_cells_y):
+        for cell_x in range(n_cells_x):
+            start_x =cell_x*grid
+            start_y =cell_y*grid
+            end_x =(cell_x+1)*grid
+            end_y =(cell_y+1)*grid
+            if np.sum(padded_map[start_y:end_y,start_x:end_x]) >(255*grid**2 -1):
+                #allowed cells
+                cv2.rectangle(image_bgr,(start_x,start_y),(end_x,end_y),(0,100,0),1)
+                state_matrix[cell_x,cell_y]=1
+            else:
+                cv2.rectangle(image_bgr,(start_x,start_y),(end_x,end_y),(0,0,100),-1)
+    cv2.imshow("grid",image_bgr)
+    
+    cv2.imwrite("/home/saleeq/catkin_ws/src/roboconvoy/data/states/grid_map.bmp",image_bgr)
+    return state_matrix
+
 image_path = "/home/saleeq/catkin_ws/src/roboconvoy/data/states/new_map.pgm"
 result = trim_whitespace(image_path)
-grid= calculate_grid_size(result)
-x_dim =result.shape(0)
-y_dim =result.shape(1)
-n_cells_x = math.ceil(x_dim/grid)
-n_cells_y = math.ceil(y_dim/grid)
-state_matrix =np.zeros(n_cells_x,n_cells_y)
+state_matrix =state_matrix_generator(result)
 
-print(grid)
 if result is not None:
     # Display the original and modified images
     cv2.imshow("Original Image", cv2.imread(image_path, cv2.IMREAD_GRAYSCALE))
--- data/states/test.py
+++ data/states/test.py
@@ -1,51 +1,16 @@
-import numpy as np
+globalVar =None
 
-def find_first_last_nonzero_indices(matrix):
-    # Convert the matrix to a NumPy array
-    matrix = np.array(matrix)
 
-    # Find the indices of non-zero elements
-    non_zero_indices = np.nonzero(matrix)
+def add_to_global_var(num:int):
+    global globalVar
+    globalVar +=num
+def set_global_var():
+    global globalVar
+    globalVar=0
 
-    if len(non_zero_indices[0]) == 0:
-        # If there are no non-zero elements, return None
-        return None
-    else:
-        # Find the first and last non-zero indices
-        first_nonzero = (non_zero_indices[0][0], non_zero_indices[1][0])
-        last_nonzero = (non_zero_indices[0][-1], non_zero_indices[1][-1])
+print(globalVar)
 
-        return first_nonzero, last_nonzero
-
-# Example usage
-matrix = [
-    [0, 0, 0, 0, 1],
-    [0, 0, 1, 0, 0],
-    [0, 1, 0, 0, 0],
-    [1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0]
-]
-
-result = find_first_last_nonzero_indices(matrix)
-
-if result is not None:
-    print("First non-zero index:", result[0])
-    print("Last non-zero index:", result[1])
-else:
-    print("No non-zero elements found in the matrix.")
-import cv2
-import numpy as np
-
-# Assuming 'binary_array' is your binary NumPy array
-binary_array = np.random.choice([0, 1], size=(100, 100))  # Replace this with your actual array
-
-# Convert the binary array to an 8-bit unsigned integer array
-image_array = binary_array.astype(np.uint8) * 255
-
-# Convert the NumPy array to a CV2 image (assuming grayscale)
-bw_image = cv2.cvtColor(image_array, cv2.COLOR_GRAY2BGR)
-
-# Display the image using OpenCV (optional)
-cv2.imshow("Converted Image", bw_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+set_global_var()
+print(globalVar)
+add_to_global_var(2)
+print(globalVar)
